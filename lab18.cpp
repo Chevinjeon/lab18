78198989340dc79f6a39aa42024ecf6e5abf8d02
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-10-05 15:07:25 -0700

    second commit

--- lab18.cpp
+++ lab18.cpp
@@ -20,3 +20,29 @@ private:
     ReviewNode* tail;
     int count;
     
+public:
+    // Constructor
+    MovieReviewList() : head(nullptr), tail(nullptr), count(0) {}
+    
+    // Destructor to clean up memory
+    ~MovieReviewList() {
+        ReviewNode* current = head;
+        while (current != nullptr) {
+            ReviewNode* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+    
+    // Add node at the head of the linked list
+    void addAtHead(double rating, string comments) {
+        ReviewNode* newNode = new ReviewNode(rating, comments);
+        newNode->next = head;
+        head = newNode;
+        
+        // If this is the first node, it's also the tail
+        if (tail == nullptr) {
+            tail = newNode;
+        }
+        count++;
+    }
